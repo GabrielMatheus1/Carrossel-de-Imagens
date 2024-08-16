487d2e6929c224a1eafde881663475127118c776
Gabriel Matheus <gabriel65451@gmail.com>
2024-08-15 21:15:00 -0300

    imgs

--- js/interatividade.js
+++ js/interatividade.js
@@ -6,22 +6,34 @@ const informacao = document.querySelectorAll(".informacao");
 // função geral para cada botao //
 botoesFlex.forEach((botao, indice) => {
 
+    // evento que todos os botões faram //
     botao.addEventListener("click", () => {
 
+        // buscara o botão com a classe //
         const botaoSelecionado = document.querySelector(".selecionado");
 
+        // removera a classe de qual estiver //
         botaoSelecionado.classList.remove("selecionado");
 
+        // add no botão que sera clicado //
         botao.classList.add("selecionado");
 
+        // mesmo esquema dos botões so que para imagens //
         const imagemselecionado = document.querySelector(".selecionada");
+
+        // removera a classe de qual estiver //
         imagemselecionado.classList.remove("selecionada");
 
+        // add a classe de acordo com o botão selecionaod com o indice //
         imagens[indice].classList.add("selecionada");
 
+        // mesmo esquema das imagens com o indice para exibir as informações //     
         const informacaoAtiva = document.querySelector(".ativa");
+
+        // removera a classe do item q tiver //
         informacaoAtiva.classList.remove("ativa");
 
+        // add a classe de acordo com o botão selecionaod com o indice //
         informacao[indice].classList.add("ativa");
     });
 });
